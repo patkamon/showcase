3461c353db84b8d1cb4fd8161aebb10194b9014d
patkamon <patkamontura@gmail.com>
2025-04-22 21:01:37 +0700

    for pages

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,25 +6,18 @@ const i18n = createI18n({
   messages: {}, // initially empty
 })
 
-const loadedPages = new Set()
 
 async function loadPageLocale(pageName, projectName=undefined) {
-  if (loadedPages.has(pageName)) return
-
   const pages = import.meta.glob('./**/*.js')
-
   const path = projectName
     ? `./${pageName}/${projectName}.js`
     : `./${pageName}.js`
-  
   const module = await pages[path]?.()
   for (const lang in module.default) {
     const langMessages = module.default[lang]
     const existing = i18n.global.getLocaleMessage(lang)
     i18n.global.setLocaleMessage(lang, { ...existing, ...langMessages })
   }
-
-  loadedPages.add(pageName)
 }
 
 export  { i18n, loadPageLocale }
