4163e0769c1ac6f0154649ec5edf39896d38b6ff
patkamon <patkamontura@gmail.com>
2025-04-22 21:13:20 +0700

    add ghost

---
+++ src/i18n/projects/whatsghost.js
@@ -0,0 +1,24 @@
+export default {
+    en: {
+      title: "Use AI to summarize the ghost",
+      idea: "Use gemini to summarize the ghost and read it aloud",
+      date: "01/25",
+      keywords: "Gemini/MongoDB/Lambda/FFmpeg/GoogleTTS",
+      source: "github/break/youtube/break/https://whatsghost.xyz/",
+      paragraph_1: "Get auto-generated captions from youtube video as text and send it to gemini to summarize it.",
+      paragraph_2: "Then send the summary to google text2speech to get the audio file.",
+      paragraph_3: "Combine it into a video using ffmpeg.",
+      paragraph_4: "Insert into mongo db and deploy lambda function so that it can be accessed from the website.",
+    },
+    th: {
+      title: "ใช้ AI สรุปเรื่องย่อ the ghost",
+      idea: "ใช้ gemini สรุปเรื่องย่อ the ghost และอ่านให้ฟัง",
+      date: "01/25",
+      keywords: "Gemini/MongoDB/Lambda/FFmpeg/GoogleTTS",
+      source: "github/break/youtube/break/https://whatsghost.xyz/",
+      paragraph_1: "ดึง auto generate caption จาก youtube video มาเป็น text ส่งให้กับ gemini เพื่อให้มันสรุปเรื่องย่อให้",
+      paragraph_2: "จากนั้นส่งสรุปให้ google text2speech เพื่อให้ไดก้ไฟล์เสียง",
+      paragraph_3: "ประกอบรวมเป็น video โดยใช้ ffmpeg",
+      paragraph_4: "insert เข้า mongo db  deploy lambda function เพื่อให้สามารถเรียกดูได้จากหน้าเว็บ",
+    },
+  }
